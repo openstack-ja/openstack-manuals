6bb698729d0739693d2775c9027d605a59256f7e
Akihiro MOTOKI <amotoki@gmail.com>
2013-07-05 16:13:34 +0900

    Use n-gram search strategy to create webhelp index
    Add <webhelpIndexerLanguage>ja</webhelpIndexerLanguage> to the configuration
    section based on the advise from David on openstack-docs ML.
    
    ----
    If you build a webhelp version, please add the following to the
    appropriate <configuration> section of the pom:
    
    <webhelpIndexerLanguage>ja</webhelpIndexerLanguage>
    
    With that set, the search should work using an n-gram search strategy,
    taking into account the fact that Japanese doesn't have spaces between
    words to use when creating an index. So it looks for any sentence in
    which that combination of characters occurs. Not perfect, but probably
    the best you can do with a JavaScript-powered client-side search.

--- tools/generate-ja-opsguide.sh
+++ tools/generate-ja-opsguide.sh
@@ -4,6 +4,7 @@ git checkout trans-ja-opsguide
 ./tools/generatedocbook -l ja -b openstack-ops
 cd generated/ja/openstack-ops/
 sed -i 's/1.7.3-SNAPSHOT/1.8.1-SNAPSHOT/' pom.xml
+sed -i -e '/webhelpDirname/a\                            <webhelpIndexerLanguage>ja</webhelpIndexerLanguage>' pom.xml
 mvn clean generate-sources
 cd -
 git checkout gh-pages
